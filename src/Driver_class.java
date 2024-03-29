9e2e4f3e29efce07aa861add31680e3672be7d3e
munirahi <muneerah.ibrahem.2016@gmail.com>
2024-03-29 20:42:03 +0300

    commit

--- src/Driver_class.java
+++ src/Driver_class.java
@@ -1,5 +1,13 @@
-public class Main {
+public class Driver_class {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
+
+    public static void n(){
+
+    }
+
+
 }
--- src/PCB.java
+++ src/PCB.java
@@ -1,2 +1,13 @@
+
 public class PCB {
+    String ProcessID ;
+    int priority ;
+    int arrival_time ; // not sure of the type
+    int CPU_burst ;
+   int Start_and_termination_time;
+   int Turn_around_time ;
+    int Waiting_time;
+   int  Response_time;
+
+
 }
